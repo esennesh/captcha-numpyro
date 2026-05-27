451ceaf9c36af366c0b96c4e40a1abfa2f3860d0
Eli Sennesh <elisennesh@astera.org>
2026-05-27 15:52:25 -0700

    src.model.ShapePlacements: combine deconvolved kernel images via compositing
    Signed-off-by: Eli Sennesh <elisennesh@astera.org>

--- src/model/model.py
+++ src/model/model.py
@@ -147,7 +147,8 @@ class ShapePlacements(nnx.Module):
 
     def __call__(self, rngs=None):
         wheres = numpyro.sample("what_x_where", self.prior(rngs=rngs))
-        return self.shaper(wheres).sum(axis=0)
+        layers = self.shaper(wheres)
+        return jnp.take(jax.lax.associative_scan(over, layers, axis=-4), -1, -4)
 
 class BackgroundDecoder(nnx.Module):
     def __init__(self, embedding_dim: int=50, height=60, hiddens=400, width=160,
